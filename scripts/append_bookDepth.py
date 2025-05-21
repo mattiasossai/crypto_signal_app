e63aeafef3352e5f889f230d0f66f43d1405da62
mattiasossai <mattiasossai@yahoo.com>
2025-05-21 17:33:12 +0200

    Update append_bookDepth.py

--- scripts/append_bookDepth.py
+++ scripts/append_bookDepth.py
@@ -3,21 +3,15 @@ import sys
 import pandas as pd
 
 """
-append_bookDepth.py
+append_bookdepth.py
 
-1) Pfad zum alten Parquet
-2) Pfad zum neuen tmp_<SYMBOL>.parquet
-3) Pfad zum Out-Parquet
-
-Fügt beide DataFrames zusammen, sortiert und schreibt mit Snappy-Kompression.
+Argumente:
+  1) Path zum alten Parquet
+  2) Path zum neuen tmp_<SYMBOL>.parquet
+  3) Path zum Out-Parquet
 """
 old_path, new_path, out_path = sys.argv[1:]
-df_old = pd.read_parquet(old_path) if old_path else None
+df_old = pd.read_parquet(old_path)
 df_new = pd.read_parquet(new_path)
-
-if df_old is not None:
-    df = pd.concat([df_old, df_new]).sort_index()
-else:
-    df = df_new
-
+df = pd.concat([df_old, df_new]).sort_index()
 df.to_parquet(out_path, compression="snappy")
