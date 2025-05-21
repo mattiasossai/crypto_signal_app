5894225d4d1c884e031f3925562e54f724300d48
mattiasossai <mattiasossai@yahoo.com>
2025-05-21 20:06:26 +0200

    Update append_bookdepth.py

--- scripts/append_bookdepth.py
+++ scripts/append_bookdepth.py
@@ -2,16 +2,11 @@
 import sys
 import pandas as pd
 
-"""
-append_bookdepth.py
-
-Argumente:
-  1) Path zum alten Parquet
-  2) Path zum neuen tmp_<SYMBOL>.parquet
-  3) Path zum Out-Parquet
-"""
+# argv[1] = alter Parquet-Pfad
+# argv[2] = tmp_<SYMBOL>.parquet
+# argv[3] = output-Pfad
 old_path, new_path, out_path = sys.argv[1:]
-df_old = pd.read_parquet(old_path)
-df_new = pd.read_parquet(new_path)
-df = pd.concat([df_old, df_new]).sort_index()
+old = pd.read_parquet(old_path)
+new = pd.read_parquet(new_path)
+df = pd.concat([old, new]).sort_index()
 df.to_parquet(out_path, compression="snappy")
